f647e90ddc29f50bf079106ef14595131d0dcf15
Yuta Tokusashi <aom@sfc.wide.ad.jp>
2018-07-17 18:38:52 +0900

    update

---
+++ lib/wombat_v1_0_0/hls/hls.tcl
@@ -0,0 +1,54 @@
+###########################################################
+#  Vivado HLS 
+#  Copyright (c) 2016 Yuta Tokusashi. All rights reserved
+###########################################################
+
+set prj "app"
+set device xc7vx690tffg1761-3
+## Target Clock Period (ns) / ?MHz
+set clock_period 12.5
+
+set src [lindex $argv 3]
+set top [lindex $argv 4]
+set mode [lindex $argv 5]
+set tb  [lindex $argv 6]
+
+### Project file initilizing ###
+open_project -reset ${prj}
+set_top ${top}
+add_files ${src}
+foreach cfile ${src} {
+	if {[string match *.c ${cfile}]} {
+		add_files ${cfile}
+	}
+}
+
+foreach tfile ${tb} {
+	if {[string match *.c ${tfile}]} {
+		add_files -tb ${tfile}
+	}
+}
+
+### Setting boards parameter ###
+open_solution -reset "solution1"
+set_part ${device}
+create_clock -period ${clock_period} -name default
+
+source "./directive.tcl"
+
+### HLS Design to RTL ###
+
+if {[string match "synth" ${mode}]} {
+	csynth_design
+} elseif {[string match "csim" ${mode}]} {
+	csim_design
+} elseif {[string match "cosim" ${mode}]} {
+	# TBD
+	csynth_design
+	cosim_design
+} elseif {[string match "export_ip" ${mode}]} {
+	# TBD
+	export_design -evaluate verilog -format ip_catalog
+}
+
+exit
