1434e8b88ebd4f26a0e403a2d00c7ae43cfac36c
Yuta Tokusashi <aom@sfc.wide.ad.jp>
2018-07-27 09:17:02 +0900

    update

---
+++ lib/wombat_v1_0_0/src/exp4_test.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ap_fixed.h>
+#include <time.h>
+#include "hls_math.h"
+#define LAYER_SIZE 3
+#define LAYER1 784
+#define LAYER2 50
+#define LAYER3 100
+#define LAYER4 10
+#define K 10
+//#define BUFSIZ 4000
+//typedef float FIX_T;
+typedef ap_fixed<32,16> FIX_T;
+
+int predict(FIX_T x[1][LAYER1], FIX_T w1[LAYER1][LAYER2], FIX_T w2[LAYER2][LAYER3],
+		FIX_T w3[LAYER3][LAYER4], FIX_T b1[LAYER2], FIX_T b2[LAYER3], FIX_T b3[LAYER4],
+		float Gamma, float p, int mode);
+
+int main()
+{
+    FIX_T x[1][LAYER1];
+    FIX_T w1[LAYER1][LAYER2], w2[LAYER2][LAYER3], w3[LAYER3][LAYER4];
+    FIX_T b1[LAYER2], b2[LAYER3], b3[LAYER4];
+    int result;
+    FILE *fp;
+    int i,j;
+    float val;
+    int t, T, r, mode;
+    float Gamma, p;
+
+    fp = fopen("/home3/okuyama/predict/one_images.txt", "r");
+    for (i = 0; i < LAYER1; i++) {
+    	fscanf(fp, "%f", &val);
+    	x[0][i] = val / 255.0;
+    }
+    fclose(fp);
+
+    fp = fopen("/home3/okuyama/predict/adam-ep10-val_loss0.10329-val_acc0.97080.txt", "r");
+    for (i = 0; i < LAYER1; i++) {
+    	for (j = 0; j < LAYER2; j++) {
+    		fscanf(fp, "%f", &val);
+    		w1[i][j] = val;
+    	}
+    }
+    for (i = 0; i < LAYER2; i++) {
+      	for (j = 0; j < LAYER3; j++) {
+      		fscanf(fp, "%f", &val);
+        	w2[i][j] = val;
+        }
+     }
+    for (i = 0; i < LAYER3; i++) {
+      	for (j = 0; j < LAYER4; j++) {
+      		fscanf(fp, "%f", &val);
+        	w3[i][j] = val;
+        }
+     }
+    for (i = 0; i < LAYER2; i++) {
+    	fscanf(fp, "%f", &val);
+    	b1[i] = val;
+    }
+    for (i = 0; i < LAYER3; i++) {
+    	fscanf(fp, "%f", &val);
+    	b2[i] = val;
+    }
+    for (i = 0; i < LAYER4; i++) {
+    	fscanf(fp, "%f", &val);
+    	b3[i] = val;
+    }
+    fclose(fp);
+
+    srand((unsigned)time(NULL));
+    T = 1;
+    Gamma = 0.5;
+
+    for (t = 0; t < T; t++) {
+    	if (t == 0)
+    		mode = 0;
+    	else
+    		mode = 1;
+    	r = rand() % 10000;
+    	p = (float)r / 10000.0;
+    	result = predict(x, w1, w2, w3, b1, b2, b3, Gamma, p, mode);
+    	printf("result = %d\n", result);
+    }
+
+	return 0;
+}
