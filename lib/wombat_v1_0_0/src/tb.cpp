b19e978815a74a9f44355a614c575e8c73a0b45c
Yuta Tokusashi <aom@sfc.wide.ad.jp>
2018-08-02 22:09:18 -0700

    update

--- lib/wombat_v1_0_0/src/tb.cpp
+++ lib/wombat_v1_0_0/src/tb.cpp
@@ -1,30 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "top.h"
-
-//typedef ap_uint<256> uint256_t;
-
 
 int main(void)
 {
-	int i;
+	int i, j = 0, p = 0;
+	uint8_t data;
 	uint256_t res, in;
 	hls::stream<uint256_t> st;
-    float val;
-	uint8_t data;
-	int p = 0;
 
-	//uint256_t td[DATASIZE] = {10,20,30,40,50,60,70,80,90,100};
+	const float gamma = 0.5;
+	const int T = 5;
+	int t,r;
+	float p;
+	int mode;
+	srand((unsigned)time(NULL));
+
 	fp = fopen("../../../../../src/one_images.txt", "r");
 	for (i = 0; i < LAYER1; i++) {
 		fscanf(fp, "%u", &data);
 		in = (in<<8) | (data & 0xff);
-		if (j == 31) {
-			td[p] = in;
-			j = 0;
-			p++;
-		} else {
-			j++;
-		}
+		if (j+1 % AXIS_BYTE_WIDTH == 0)
+			td[j%AXIS_BYTE_WIDTH] = in;
+		j++;
 	}
 	fclose(fp);
 
@@ -32,10 +30,16 @@ int main(void)
 		st.write(td[i]);
 	}
 
-	res = sample(st);
-	int ress = (int)res;
-
-	printf("Result: %d \n", ress);
+	for (t = 0; t < T; t++) {
+		if (t == 0)
+			mode = 0;
+		else
+			mode = 1;
+		r = rand() % 10000;
+		p = (float)r / 10000.0;
+		res = sample(st, gamma. p mode);
+		printf("Result: %d \n", res);
+	}
 
 	return 0;
 }
