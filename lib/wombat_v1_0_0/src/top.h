ff9ffb4b64dd46a3dd483b5c8b0903ea61d7dfec
Yuta Tokusashi <aom@sfc.wide.ad.jp>
2018-08-02 22:17:20 -0700

    update

--- lib/wombat_v1_0_0/src/top.h
+++ lib/wombat_v1_0_0/src/top.h
@@ -11,4 +11,4 @@
 
 typedef ap_uint<256> uint256_t;
 typedef ap_fixed<32,16> FIX_T;
-uint256_t sample(hls::stream<uint256_t>& st);
+uint256_t sample(hls::stream<uint256_t>& st, float gamma, float p, int mode);
